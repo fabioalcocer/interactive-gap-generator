d85266fc938bdf8ca0c182e6d09c639dbcb2046a
Fabio Alcocer <fabioalcocersejas17@gmail.com>
2022-05-05 01:24:35 -0400

    fix some bugs

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const grid = document.querySelector('.container__grid')
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
 
-    if (button.classList.contains("active")) {
-      button.classList.toggle("active");
-    } else {
+    if (!button.classList.contains("active")) {
       buttons.forEach((button) => button.classList.remove("active"));
       button.classList.add("active");
     }
 
     grid.style.gap = e.target.dataset.gap
-
   })
 })
 
