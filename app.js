0dea3343c4ac75bfa205495a80e62ae4e2ae257c
Fabio Alcocer <fabioalcocersejas17@gmail.com>
2022-05-05 01:07:04 -0400

    finish project

---
+++ app.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll('.nav__button')
+const grid = document.querySelector('.container__grid')
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+
+    if (button.classList.contains("active")) {
+      button.classList.toggle("active");
+    } else {
+      buttons.forEach((button) => button.classList.remove("active"));
+      button.classList.add("active");
+    }
+
+    grid.style.gap = e.target.dataset.gap
+
+  })
+})
+
+
